58ed72e00da879617ccfa2e9bca20327b00a0424
Alan Agius <alan.agius4@gmail.com>
2018-03-23 14:37:12 +0100

    add templating engine

--- server.ts
+++ server.ts
@@ -8,6 +8,8 @@ import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
 
 import * as express from 'express';
 import {join} from 'path';
+import {readFileSync} from 'fs';
+import {render} from 'mustache';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -16,18 +18,22 @@ enableProdMode();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+const SSR_ENABLED = process.env.SSR === 'false';
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main.bundle');
+const viewEngine = SSR_ENABLED
+  ? ngExpressEngine({
+    bootstrap: AppServerModuleNgFactory,
+    providers: [
+      provideModuleMap(LAZY_MODULE_MAP)
+    ]
+  })
+  : (_req, res, cb) => cb(null, res.document);
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
+app.engine('html', viewEngine);
 
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
@@ -41,9 +47,30 @@ app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
   maxAge: '1y'
 }));
 
-// ALl regular routes use the Universal engine
+const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html'))
+  .toString()
+  // we need to do this due to the fact that we don't have JS to use the 'template'
+  .replace(/<\/?template>/g, '');
+
 app.get('*', (req, res) => {
-  res.render('index', { req });
+  res.render('index', {
+    req,
+    res,
+    document: render(
+      template,
+      {
+        // these are from API
+        containerID: 1235,
+        brand: 'wildfire',
+        platform: 'desktop'
+      },
+      {
+        // possible partial from external file, this can be populated via readFile
+        // optimizlyContainer: optimizlyEnabled ? optimizleCacheTemplate : noop(),
+        partial: 'hello from {{brand}}'
+      }
+    )
+  });
 });
 
 // Start up the Node server
